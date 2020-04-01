5c9d934bee4b8b022b99ddff49a8159b5a4b5616
nipindasani3 <60406003+nipindasani3@users.noreply.github.com>
2020-04-01 05:14:54 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Assignment4.Models;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Word.CreateWordprocessingDocument($"{ Constants.Locations.DataFolder}\\info.docx");
+
+            Excel.CreateSpreadsheetWorkbook($"{ Constants.Locations.DataFolder}\\info.xlsx");
+            Excel.InsertText($"{ Constants.Locations.DataFolder}\\info.xlsx");
+
+            ppt.CreatePresentation(@"C:\Users\Nipin's HP\Desktop\info.pptx");
+            insertPpt.InsertNewSlide(@"C:\Users\Nipin's HP\Desktop\info.pptx", 1, "My new slide");
+
+
+        }
+    }
+}
